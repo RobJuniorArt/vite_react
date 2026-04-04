6f4fb06df2f5863c25c415c8ad930cb857c350d2
Rob Junior <robson.fsj@gmail.com>
2026-04-04 19:23:15 -0300

    feat: setup inicial

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import prettier from 'eslint-plugin-prettier/recommended'; // Import correto do prettier
+
+export default [
+  { ignores: ['dist'] }, // Substitui o globalIgnores antigo
+  js.configs.recommended,
+  prettier, // Isso já desativa os conflitos e ativa o prettier
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'prettier/prettier': 'error', // Faz o erro do prettier aparecer no ESLint
+    },
+  },
+];
